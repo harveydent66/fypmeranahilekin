7f7839c19b1eb16bc0f716d6d5f6882f9e5a6353
harveydent66 <95881273+harveydent66@users.noreply.github.com>
2024-10-20 18:52:15 +0500

    Update RecommendSystem_app.py

--- RecommendSystem_app.py
+++ RecommendSystem_app.py
@@ -15,59 +15,17 @@ games_df, similarity_df = getdata()[0], getdata()[1]
 
 #<span style="color: red;">Text with red color</span>', unsafe_allow_html=True
 # Sidebar
-# Sidebar
-st.sidebar.markdown('<strong><span style="color: #8B2500;font-size: 26px;"> Game recommendation</span></strong>', unsafe_allow_html=True)
+st.sidebar.markdown('<strong><span style="color: #8B2500;font-size: 26px;"> Game recommendation</span></strong>',unsafe_allow_html=True)
+
 st.sidebar.markdown('An app by [Long Do](https://doophilong.github.io/Portfolio/)')
 st.sidebar.image('pexels-pixabay-275033.jpg', use_column_width=True)
-st.sidebar.markdown('<strong><span style="color: #EE4000;font-size: 26px;">:slot_machine: Choose your game !!!</span></strong>', unsafe_allow_html=True)
-
-# Filters in the Sidebar
-# 1. Filter by Genre
-genres = games_df['Genre'].unique().tolist()
-selected_genres = st.sidebar.multiselect('Filter by Genre(s)', genres)
-
-# 2. Filter by Developer
-developers = games_df['Developer'].unique().tolist()
-selected_developers = st.sidebar.multiselect('Filter by Developer(s)', developers)
-
-# 3. Filter by Publisher
-publishers = games_df['Publisher'].unique().tolist()
-selected_publishers = st.sidebar.multiselect('Filter by Publisher(s)', publishers)
-
-# 4. Filter by Release Year Range
-min_year = int(games_df['Release Year'].min())
-max_year = int(games_df['Release Year'].max())
-selected_year_range = st.sidebar.slider('Filter by Release Year', min_value=min_year, max_value=max_year, value=(min_year, max_year))
-
-# Apply filters
-filtered_games = games_df.copy()
-
-# Apply genre filter if any genres are selected
-if selected_genres:
-    filtered_games = filtered_games[filtered_games['Genre'].isin(selected_genres)]
-
-# Apply developer filter if any developers are selected
-if selected_developers:
-    filtered_games = filtered_games[filtered_games['Developer'].isin(selected_developers)]
-
-# Apply publisher filter if any publishers are selected
-if selected_publishers:
-    filtered_games = filtered_games[filtered_games['Publisher'].isin(selected_publishers)]
-
-# Apply release year filter
-filtered_games = filtered_games[
-    (filtered_games['Release Year'] >= selected_year_range[0]) &
-    (filtered_games['Release Year'] <= selected_year_range[1])
-]
-
-# Game Selection Dropdown
+st.sidebar.markdown('<strong><span style="color: #EE4000;font-size: 26px;">:slot_machine: Choose your game !!!</span></strong>',unsafe_allow_html=True)
 ph = st.sidebar.empty()
-selected_game = ph.selectbox(
-    'Select one among the filtered games (you can type it as well):',
-    [''] + filtered_games['Title'].to_list(), 
-    key='default',
-    format_func=lambda x: 'Select a game' if x == '' else x
-)
+selected_game = ph.selectbox('Select one among the 787 games '
+                             'from the menu: (you can type it as well)',
+                             [''] + games_df['Title'].to_list(), key='default',
+                             format_func=lambda x: 'Select a game' if x == '' else x)
+
 # Recommendations
 if selected_game:
 
